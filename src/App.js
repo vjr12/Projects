aa6b337f2570d9d2e8bc6bb22402b6c9b2042c87
Velson Rosario <velsonrosario@Velsons-MacBook-Pro-13.local>
2022-12-26 01:04:36 +0530

    pagination commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useState } from "react";
+import "./styles.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchProducts = async () => {
+    const response = await fetch("https://dummyjson.com/products/?limit=40");
+    const json = await response.json();
+    if (!!json && json?.products?.length) setProducts(json?.products);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const handlePageClick = (index) => {
+    if (index >= 1 && index <= products.length / 10 && page !== index)
+      setPage(index);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!!products.length && (
+        <div className="products">
+          {products
+            .slice(page * 10 - 10, page * 10)
+            .map(({ id, thumbnail, title }) => {
+              return (
+                <span className="product" key={id}>
+                  <img src={thumbnail} alt={title} />
+                  <span>{title}</span>
+                </span>
+              );
+            })}
+        </div>
+      )}
+      {!!products.length && (
+        <div className="pagination">
+          <span
+            className={page <= 1 ? "disabled" : ""}
+            onClick={() => handlePageClick(page - 1)}
+          >
+            &#60;
+          </span>
+          {[...Array(products.length / 10)].map((_, i) => {
+            return (
+              <span
+                className={i + 1 === page ? "selected" : ""}
+                onClick={() => handlePageClick(i + 1)}
+                key={i}
+              >
+                {i + 1}
+              </span>
+            );
+          })}
+          <span
+            className={page >= products.length / 10 ? "disabled" : ""}
+            onClick={() => handlePageClick(page + 1)}
+          >
+            &#62;
+          </span>
+        </div>
+      )}
     </div>
   );
 }
